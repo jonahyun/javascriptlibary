7c59dfb3bacacdf663eeb66b97840713b4194f0f
Jona Hyun <jona.hyun@gmail.com>
2017-01-16 23:41:20 -0500

    messing with json trying to get it work

--- 0.16_JSON_Loading.js
+++ 0.16_JSON_Loading.js
@@ -19,9 +19,10 @@
     request.onload = function() {
       var superHeroesText = request.response;
       var superHeroes = JSON.parse(superHeroesText);
-      populateHeader(superHeroes);
+      //populateHeader(superHeroes);
       showHeroes(superHeroes);
     }
+    /*
     function populateHeader(jsonObj) {
       var myH1 = document.createElement('h1');
       myH1.textContent = jsonObj['squadName'];
@@ -30,6 +31,7 @@
       myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
       header.appendChild(myPara);
     }
+    */
 
 // display the information
 
@@ -38,27 +40,28 @@
       
       for(i = 0; i < heroes.length; i++) {
         var myArticle = document.createElement('article');
-        var myH2 = document.createElement('h2');
+        //var myH2 = document.createElement('h2');
         var myPara1 = document.createElement('p');
         var myPara2 = document.createElement('p');
         var myPara3 = document.createElement('p');
-        var myList = document.createElement('ul');
-        myH2.textContent = heroes[i].name;
+        //var myList = document.createElement('ul');
+        //myH2.textContent = heroes[i].name;
         myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
         myPara2.textContent = 'Age: ' + heroes[i].age;
         myPara3.textContent = 'Superpowers:';
-        
+        /*
         var superPowers = heroes[i].powers;
         for(j = 0; j < superPowers.length; j++) {
           var listItem = document.createElement('li');
           listItem.textContent = superPowers[j];
           myList.appendChild(listItem);
         }
-        myArticle.appendChild(myH2);
+        */
+        //myArticle.appendChild(myH2);
         myArticle.appendChild(myPara1);
         myArticle.appendChild(myPara2);
         myArticle.appendChild(myPara3);
-        myArticle.appendChild(myList);
+        //myArticle.appendChild(myList);
         section.appendChild(myArticle);
       }
     }
